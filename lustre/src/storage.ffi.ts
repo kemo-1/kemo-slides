0523a715123d31053d25d69492bac02c9bbd1755
sanabel-al-firdaws <abdullhakim.sami@gmail.com>
2025-01-09 21:09:36 +0300

    its working now

--- lustre/src/storage.ffi.ts
+++ lustre/src/storage.ffi.ts
@@ -1,140 +1,89 @@
-import { LoroDoc } from 'loro-crdt/base64'
 import { fromUint8Array, toUint8Array } from 'js-base64';
-const doc = new LoroDoc();
+import * as Y from "yjs"
+import { IndexeddbPersistence } from 'y-indexeddb'
+import * as awarenessProtocol from 'y-protocols/awareness.js'
 
-const lorodocument = localStorage.getItem('loro')!
-if (lorodocument) {
-    let bytes = toUint8Array(lorodocument)
 
-    doc.import(bytes)
-}
+const yDoc = new Y.Doc()
+const awareness = new awarenessProtocol.Awareness(yDoc)
 
+const documentName = ''
 
 const serverUrl = 'localhost:8000'
-const documentName = ''
+
 const socket = new WebSocket(`ws://${serverUrl}/api/${documentName}`)
-doc.subscribe((update => {
-
-    if (socket.readyState === WebSocket.OPEN) {
-        let bytes = doc.export({ mode: "snapshot" })
-        let string = fromUint8Array(bytes)
-
-        let json_document = { doc: string }
-        let json = JSON.stringify(json_document)
-        socket.send(json)
-
-        let document_array = doc.toJSON().notes
 
 
-        let event_change = new CustomEvent('content-update', {
-            detail: {
-                notes: document_array
-            },
+const provider = new IndexeddbPersistence(documentName, yDoc)
+//@ts-ignore
+provider.awareness = awareness
 
-            bubbles: true, // Allows the event to bubble up the DOM
-            composed: true, // Allows the event to cross the shadow DOM boundary (if present)
-        })
-        document.getElementById("notes-container")!.dispatchEvent(event_change)
-
-    }
-
-
-}))
-// socket.onmessage( f{})
+const yarray = yDoc.getArray('root')
 
 
 socket.onmessage = (event) => {
     let json = JSON.parse(event.data)
-    let document = json.doc
+    let doc = json.doc
+    let awareness = json.awareness
 
-    let bytes = toUint8Array(document)
-
-    doc.import(bytes)
-
-    localStorage.setItem('loro', document)
-
-
-
-    // let custom_event = new CustomEvent("doc-update")
+    if (doc !== undefined) {
+        const binaryEncoded = toUint8Array(doc)
+        Y.applyUpdate(yDoc, binaryEncoded)
+    }
+    if (awareness !== undefined) {
+        const binaryEncoded = toUint8Array(awareness)
+        //@ts-ignore
+        awarenessProtocol.applyAwarenessUpdate(provider.awareness, binaryEncoded, '')
+    }
 }
 
-
-
-const movableList = doc.getMovableList("notes");
-
-export function insert_note(word: string) {
-    // word.replaceAll(/\/\//g, "/")   
-    let document_json = doc.toJSON()
-
-
-    movableList.insert(0, word)
-
-
-    let bytes = doc.export({ mode: "snapshot" })
-    let string = fromUint8Array(bytes)
-
-    localStorage.setItem('loro', string)
-
+yDoc.on('update', () => {
+    const documentState = Y.encodeStateAsUpdate(yDoc)
+    const binaryEncoded = fromUint8Array(documentState)
 
     if (socket.readyState === WebSocket.OPEN) {
-        let bytes = doc.export({ mode: "snapshot" })
-        let string = fromUint8Array(bytes)
-
-        let document_json = { doc: string }
-        let json = JSON.stringify(document_json)
+        let doc = { doc: binaryEncoded }
+        let json = JSON.stringify(doc)
         socket.send(json)
     }
+    let event_change = new CustomEvent('content-update', {
+        detail: {
+            notes: yarray.toArray()
+        },
+
+        bubbles: true, // Allows the event to bubble up the DOM
+        composed: true, // Allows the event to cross the shadow DOM boundary (if present)
+    })
+    document.getElementById("notes-container")!.dispatchEvent(event_change)
+
+})
+//@ts-ignore
+provider.awareness.on('update', ({ added, updated, removed }) => {
+    if (socket.readyState === WebSocket.OPEN) {
+        const changedClients = added.concat(updated).concat(removed)
+        //@ts-ignore
+        const documentAwareness = awarenessProtocol.encodeAwarenessUpdate(provider.awareness, changedClients)
+        const binaryEncoded = fromUint8Array(documentAwareness)
+
+        let awareness = { awareness: binaryEncoded }
+        let json = JSON.stringify(awareness)
+        socket.send(json)
+    }
+})
 
 
-
-
-
-
-    return document_json.notes
+export function insert_note(word: string) {
+    yarray.insert(0, [word])
+    return yarray.toArray()
 }
 
 export function delete_note(index: number) {
+    yarray.delete(index, 1)
 
-    let document_json = doc.toJSON()
-
-    movableList.delete(index, 1)
-
-
-    let bytes = doc.export({ mode: "snapshot" })
-    let string = fromUint8Array(bytes)
-    localStorage.setItem('loro', string)
-
-
-
-    if (socket.readyState === WebSocket.OPEN) {
-        let bytes = doc.export({ mode: "snapshot" })
-        let string = fromUint8Array(bytes)
-
-        let document_json = { doc: string }
-        let json = JSON.stringify(document_json)
-        socket.send(json)
-    }
-
-
-    return document_json.notes
+    return yarray.toArray()
 }
 
 export function get_notes() {
-    let array = doc.toJSON().notes
-    return array
-
+    return yarray.toArray()
 }
 
-// doc.subscribeLocalUpdates((update => {
-
-//     if (socket.readyState === WebSocket.OPEN) {
-//         let string = fromUint8Array(update)
-
-//         let document = { doc: string }
-//         let json = JSON.stringify(document)
-//         doc.import(update)
-//         socket.send(json)
-//     }
-
-
-// }))
